102fc935001a033c11c3c3a451efba7d77ecb793
Jakyln <kalaycijulien@gmail.com>
2024-05-15 14:49:37 +0200

    reponse du lmm brut

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import pkg from "@lmstudio/sdk";
+import express from 'express';
+
+//const express = require('express')
+
+const { LMStudioClient } = pkg;
+
+const app = express();
+const port = 3000;
+const client = new LMStudioClient();
+
+// Load a model
+const gemma2b = await client.llm.load("lmstudio-ai/gemma-2b-it-GGUF");
+
+// Create a text completion prediction
+const prediction = gemma2b.complete("The meaning of life is");
+
+let str = "";
+// Stream the response
+/* for await (const text of prediction) {
+  //process.stdout.write(text);
+  //str += text;
+} */
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(`${str}`)
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}...`)
 })
-
