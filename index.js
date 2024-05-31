63389cf4adde263d46c821782f32810289495999
julien <kalaycijulien@gmail.com>
2024-05-31 10:27:24 +0200

    ajout des commentaires et clean

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.json())
 
-// Load models
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 // Configuration requise pour GoogleGenerativeAI
@@ -38,33 +37,21 @@ const safetySettings = [
   },
 ];
 
+// initialisation de modèle
 const geminiId = "gemini-1.5-flash";
 const geminiFlash = configuration.getGenerativeModel({ model: geminiId, safetySettings });
-
-/* import bodyParserpackage from "body-parser";
-const bodyParser = bodyParserpackage;
-import { generateResponse } from "./controller/index.js";
-
-//middleware to parse the body content to JSON
-app.use(bodyParser.json());
-
-app.post("/generate", generateResponse);
-
-app.get("/generate", (req, res) => {
-  res.send(history);
-});
- */
-
-
-
-
-
-
 
 // Create a text completion prediction
 let jsonAPI = "";
 let readableJson = "";
 
+
+
+// Fonction qui recherche un titre de film dans la phrase passé en paramètre pour récupérer des information
+// via l'api TMDB. Le LLM cherche le film qui correspond le mieux dans la liste et génère un résultat
+// qui sera retourné à l'utilisateur.
+// Si l'api TMBD n'a pas retourné de résultat ou si aucun titre de film n'a été trouvé dans la phrase
+// passé en paramètre, alors on retourne une réponse indiquant ce qui a échoué.
 async function llm(query) {
 
   //Isoler le nom du film à partir du user input
@@ -75,13 +62,10 @@ async function llm(query) {
   Si tu ne trouve pas de titre de film dans la réponse, répond par une chaine vide "".
   La phrase est : "${query}"`;
   const generatedMovieName = await geminiFlash.generateContent(promptMovieName);
-  //répond moi en json, formater ex : {reponse : tenet}
-  //connecter à chatgpt. il verif qu'on a bien extrait le nom dans la bonne langue, sans erreurs
   const movieNameResponse = await generatedMovieName.response;
   const movieName = movieNameResponse.text();
-
-
-
+  
+  // Si pas de titre de film, répondre que l'on a pas trouvé
   if (movieName.trim() === '""') {
     readableJson = "Je n'ai pas trouvé de nom de film dans votre requête."
     return
@@ -93,7 +77,7 @@ async function llm(query) {
     await MovieDataService.findMovieByName(movieName).then((res) => {
       process.stdout.write(`|${movieName}\n`);
       movieArrayJSON = res.data;
-      //connecter à chatgpt. Si L'api renvoie plusieurs résultats, on prend une map avec l'index et le nom du film. On demande à chat gpt d'analyser la prompt user initial et de ressortir le bon id. On choisit celui ci dans jsonapi
+      //Si L'api renvoie plusieurs résultats, on fait donne la liste à Gemini pour qu'il l'analyse et ressorte le bon titre
       for (let index = 0; index < res.data.results.length; index++) {
         const element = res.data.results[index];
         movieArraySimplified.push(element.title);
@@ -126,8 +110,6 @@ async function llm(query) {
     jsonAPI = movieArrayJSON.results[indexRightMovie];
 
     //LLM traduit en textuel le json
-    //Bonjour, j'aime beacoup Tenet, c'est mon film préféré !
-    //Ne génère pas de code Python, uniquement du HTML formaté selon les instructions ci-dessous
     const promptReadableJson = `
     A partir de l'objet JSON que je vais t'envoyer, dis moi ce que tu sais du film. 
     Evidemment, je ne veux pas que tu m'énumère les propriétés de l'objet JSON en lui même. 
@@ -136,6 +118,7 @@ async function llm(query) {
     const response = await predictionOfReadableJson.response;
     readableJson = response.text();
 
+    // Si l'api TMBD n'a pas renvoyé de résultat, on répond que l'on a pas trouvé
     if (jsonAPI === undefined) {
       readableJson = "Je n'ai pas trouvé d'information à propos du film que vous m'avez donné."
       return
@@ -143,7 +126,7 @@ async function llm(query) {
   }
 }
 
-// Index page
+// Index page (utilisé pour les tests)
 app.get('/', async (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'))
 })
@@ -153,6 +136,8 @@ app.listen(port, () => {
 })
 
 
+// Endpoint qui traite l'entrée de l'utilisateur pour chercher quel film est mentionné
+// et renvoyer un prompt généré à partir des résultats fournis par l'api TMDB
 app.post('/request', async (req, res) => {
   if (req.body.query) {
     readableJson = "";
@@ -170,30 +155,6 @@ On lance un prompt pour dire aux llm de ressortir uniquement le nom du film à p
 Avec ce nom de film on fait un appel API TMDB pour aller chercher le json des infos de ce film
 On envoie ce json
 
--- futur
-On cache la réponse llm completion
-on lance ensuite une autre réponse llm completion, avec comme contexte une demande de ressortir en mode language le fichier json recu de l'api
-on lance ensuite une réponse llm chatbot, avec comme contexte, le résultat de la réponse précédante
-*/
-
-
-
-
-/*
-const predictionChatbot = gemma2b.respond([
- { role: "system", content: "Bonjour, je suis une IA experte en films. Parlons de film ensemble !" },
- { role: "user", content: `${movieName}` },
- { role: "system", content:  },
- { role: "user", content: "voiture" },
- { role: "system", content: "Je ne connais pas ce film. Peut tu m'en dire un autre ?" },
- { role: "user", content: query },
-]);
-
-for await (const text of predictionReadableJson) {
- readableJson += text;
-}
-
-Faire un schéma du resumé suivant
-
 A partir d'un input utilisateur parlant d'un film, le llm présente les données du film à partir du JSON d'une api distante
- */
+
+*/
